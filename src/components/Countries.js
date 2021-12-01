922972d9b94a2d53aa08c0714eb5f7b4208febee
MathiasBenmansour <mathias70250@gmail.com>
2021-12-01 16:05:27 +0100

    ayéééé j'use l'api!!!

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import { useState } from 'react'
+import axios from 'axios';
 
 const Countries = () => {
-    const [data,setData] = useState(['Hello']);
-    const sayGoodbye = () => {
-        setData("Goodbye")
-    }
+    const [data,setData] = useState([]);
+   
+    useEffect(() => {
+       axios.get("https://restcountries.com/v3.1/all")
+       .then(res => {
+        console.log(res.data)
+        setData(res.data)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+
+      
+    
+    }, []);
+   
+     
     return (
-        <div>
-        {data} 
-        <button onClick={sayGoodbye}>Dire adios</button>
+        <div className="countries">
+            <ul className ="countries-list">
+               {data.map((country) => (
+                   <li key={country.name.common}>{country.name.common} </li>
+        ))} 
+            </ul>
         </div>
     );
 };
